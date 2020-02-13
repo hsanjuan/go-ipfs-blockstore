239c549159740ced057b796a4834b3d3126c016d
Hector Sanjuan <code@hector.link>
2020-02-13 18:51:04 +0100

    mm

--- blockstore.go
+++ blockstore.go
@@ -8,12 +8,12 @@ import (
 	"sync"
 	"sync/atomic"
 
+	dshelp "github.com/hsanjuan/go-ipfs-ds-help"
 	blocks "github.com/ipfs/go-block-format"
 	cid "github.com/ipfs/go-cid"
 	ds "github.com/ipfs/go-datastore"
 	dsns "github.com/ipfs/go-datastore/namespace"
 	dsq "github.com/ipfs/go-datastore/query"
-	dshelp "github.com/ipfs/go-ipfs-ds-help"
 	logging "github.com/ipfs/go-log"
 )
 
